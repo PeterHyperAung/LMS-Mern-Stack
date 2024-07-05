40e65b4bc7e802f141d240584e7ae6bf1044ce35
Min Phone Aung <88699874+PeterHyperAung@users.noreply.github.com>
2024-07-06 00:44:09 +0630

    catch async function fixed

--- server/app.ts
+++ server/app.ts
@@ -19,12 +19,12 @@ app.get("/test", (req: Request, res: Response, next: NextFunction) => {
 
 app.use("/api/v1/auth", authRouter);
 
+// Error route
+app.use(errorMiddleware);
+
 // unknown route
 app.all("*", (req: Request, res: Response, next: NextFunction) => {
   const err = new Error(`Route ${req.originalUrl} not found`) as any;
   err.statusCode = 404;
   next(err);
 });
-
-// Error route
-app.use(errorMiddleware);
--- server/utils/catchAsync.ts
+++ server/utils/catchAsync.ts
@@ -2,5 +2,5 @@ import type { NextFunction, Request, Response } from "express";
 
 export const catchAsync =
   (fn: any): any =>
-  (err: any, req: Request, res: Response, next: NextFunction) =>
+  (req: Request, res: Response, next: NextFunction) =>
     fn(req, res, next).catch(next);
